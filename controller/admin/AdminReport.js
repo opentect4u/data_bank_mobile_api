7ad4ea480ef4832907473dfdfd484a7ae114d04b
Amit Mondal <amit.ndk3@gmail.com>
2023-12-28 10:30:57 +0530

    add new reoprt summary report in branch

--- controller/admin/AdminReport.js
+++ controller/admin/AdminReport.js
@@ -208,4 +208,77 @@ const account_type_wise_report_post = async(req,res)=>{
         });
     }
 }
-module.exports={day_scroll_report,day_scroll_report_post,account_type_wise_report,account_type_wise_report_post,individual_day_scroll_report,individual_day_scroll_report_post}
+
+
+
+const summary_report = async(req,res)=>{      
+    const datetime = dateFormat(new Date(), "yyyy-mm-dd")
+    const user_data = req.session.user.user_data.msg[0];
+    var whrDAta = `bank_id='${user_data.bank_id}' AND branch_code='${user_data.branch_code}'  AND active_flag='Y'AND user_type='O'`,
+        selectData = "user_id";
+    let dbuser_data = await db_Select(selectData, "md_user", whrDAta, null);
+    var viewData = {
+        title: "Day Scroll Report",
+        page_path: "/report/summary_report/input",
+        data: dbuser_data.msg,
+        nowdate:datetime
+    };
+    res.render('common/layouts/main', viewData)
+}
+
+
+const summary_report_post = async(req,res)=>{
+    try {
+        const schema = Joi.object({
+            agent_code: Joi.string().required(),
+            // account_type: Joi.string().valid('D', 'R', 'L').required(),
+            // product_code: Joi.string().required(),
+            // from_date: Joi.string().required(),
+            // to_date: Joi.string().required(),
+        });
+        const { error, value } = schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = {};
+            error.details.forEach(detail => {
+                errors[detail.context.key] = detail.message;
+            });
+            return res.json({ error: errors });
+        }
+        const user_data = req.session.user.user_data.msg[0];
+        let select = "a.agent_trans_no,a.agent_code,a.send_date,a.received_date,a.end_flag, sum(b.deposit_amount)deposit_amount",
+            where = `a.agent_trans_no = b.agent_trans_no and a.agent_code = b.agent_code AND a.bank_id=${user_data.bank_id} AND a.branch_code='${user_data.branch_code}' AND a.agent_code='${value.agent_code}' `;
+            //AND transaction_date BETWEEN '${value.from_date}' AND '${value.to_date}'
+            let order=`group by a.agent_trans_no,a.agent_code,a.send_date,a.received_date,a.end_flag
+            order by a.send_date`;
+
+        let resData = await db_Select(select, "md_agent_trans a,td_collection b", where, order);
+
+        let resData2 = await db_Select('*', "md_agent", `bank_id=${user_data.bank_id} AND branch_code='${user_data.branch_code}' AND agent_code='${value.agent_code}'`, null);
+
+        console.log("resData============",resData2);
+        delete resData.sql
+        var viewData = {
+            title: "Day Scroll Report",
+            page_path: "/report/summary_report/report",
+            data: resData.msg,
+            // fdate:value.from_date,
+            // tdate:value.to_date,
+            datetime : dateFormat(new Date(), "dd-mm-yyyy hh:MM:ss"),
+            agent_info:resData2.msg[0]
+        };
+
+        console.log("Starting Date",viewData);
+        res.render('common/layouts/main', viewData)
+
+
+    } catch (error) {
+        res.json({
+            "success": error,
+            "status": false
+        });
+    }
+}
+
+
+
+module.exports={day_scroll_report,day_scroll_report_post,account_type_wise_report,account_type_wise_report_post,individual_day_scroll_report,individual_day_scroll_report_post,summary_report,summary_report_post}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,7 @@ const { dashboard } = require("../controller/admin/Dashboard");
 const { AuthCheckedMW, logout } = require("../middleware/AuthCheckedMW");
 const { upload_pctx, upload_pctx_file_data, download_pcrx, download_pcrx_file, create_agent_trans, show_upload_account, fetch_show_account, del_all_pctx_file_data, fetchdata_to_server, fetch_pcrx_file, update_agent_amount } = require("../controller/admin/UploadDownloadTxtFile");
 const { check_and_collection, settings } = require("../controller/admin/AdminTransaction");
-const { day_scroll_report, day_scroll_report_post, account_type_wise_report, account_type_wise_report_post, individual_day_scroll_report_post, individual_day_scroll_report } = require("../controller/admin/AdminReport");
+const { day_scroll_report, day_scroll_report_post, account_type_wise_report, account_type_wise_report_post, individual_day_scroll_report_post, individual_day_scroll_report, summary_report, summary_report_post } = require("../controller/admin/AdminReport");
 const { agent_info, fetchtransNumber, fetch_account } = require("../controller/admin/FetchData");
 const { agent, addAgent, editAgent, edit_save_agent, checkedUnicUser, fetch_agent_max_all_col, fetch_agent_name } = require("../controller/admin/Agent.controller");
 
@@ -47,6 +47,10 @@ AdminRoutes.post('/check_and_collection',AuthCheckedMW,check_and_collection)
 AdminRoutes.get('/day_scroll_report',AuthCheckedMW,day_scroll_report)
 AdminRoutes.post('/day_scroll_report_post',AuthCheckedMW,day_scroll_report_post)
 
+
+ AdminRoutes.get('/summary_report',AuthCheckedMW,summary_report)
+ AdminRoutes.post('/summary_report_post',AuthCheckedMW,summary_report_post)
+
 AdminRoutes.get('/individual_day_scroll_report',AuthCheckedMW,individual_day_scroll_report)
 AdminRoutes.post('/individual_day_scroll_report_post',AuthCheckedMW,individual_day_scroll_report_post)
 AdminRoutes.post('/fetch_account',AuthCheckedMW,fetch_account)
@@ -68,7 +72,8 @@ AdminRoutes.post('/checkedUnicUser',AuthCheckedMW,checkedUnicUser)
 AdminRoutes.get('/edit_agent/:agent_id',AuthCheckedMW,editAgent)
 AdminRoutes.post('/update_agent/:agent_id',AuthCheckedMW,edit_save_agent)
 
-AdminRoutes.get('/settings',AuthCheckedMW,settings)
+
 // AdminRoutes.get('/edit_settings/:agent_id',AuthCheckedMW,editSetting)
+
 
 module.exports={AdminRoutes};
