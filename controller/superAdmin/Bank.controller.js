bd210bd2d5e591c971fd41be436ebcb71337fc67
Sayantika <sayantika@synergicsoftek.in>
2024-01-03 18:57:19 +0530

    commit

--- controller/superAdmin/Bank.controller.js
+++ controller/superAdmin/Bank.controller.js
@@ -72,4 +72,70 @@ const add_bank_list = async (req, res) => {
 
 
 }
-module.exports = { bank_list, add_bank_list }
+
+const edit_bank_list = async (req, res) => {
+    var data = await db_Select('*', 'md_bank', `bank_id=${req.query.bank_id}`, null)
+    // console.log(data, 'lalal');
+    const viewData = {
+        title: "Adminn",
+        page_path: "/bank/edit_viewBank",
+        data: data.suc > 0 && data.msg.length > 0 ? data.msg[0] : [],
+        bank_id: req.query.bank_id,
+    };
+    res.render('common/layouts/main', viewData)
+}
+const edit_bank_list_save = async (req, res) => {
+    try {
+        const schema = Joi.object({
+            bank_name: Joi.string().required(),
+            contact_person: Joi.string().required(),
+            mobile: Joi.number().required(),
+            bank_address: Joi.string().required(),
+            email: Joi.string().required(),
+            device_type: Joi.string().required(),
+            data_version: Joi.string().valid('S', 'C', 'N').required(),
+            data_transfer_type: Joi.string().valid('M', 'A').required(),
+            receipt_type: Joi.string().valid('S', 'P', 'B').required(),
+            active_flag: Joi.string().valid('Y', 'N').required(),
+        });
+        const { error, value } = schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = {};
+            error.details.forEach(detail => {
+                errors[detail.context.key] = detail.message;
+            });
+
+            // req.flash('error', errors)
+            res.redirect('/super-admin/bank')
+
+            // return res.status(400).json({ error: errors });
+        }
+
+        const datetime = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+        const user_data = req.session.user.user_data.msg[0];
+        // console.log(user_data);
+        var table_name = 'md_bank',
+        fields = `bank_name ='${value.bank_name}', bank_address = '${value.bank_address}', contact_person = '${value.contact_person}', phone_no = '${value.mobile}', email_id ='${value.email}', device_type = '${value.device_type}', data_version = '${value.data_version}', data_trf = '${value.data_transfer_type}', 
+        receipt_type = '${value.receipt_type}', sec_amt_type = '${value.sucurity_amt_type}', active_flag = '${value.active_flag}', after_maturity_coll = '${value.after_maturity_coll}', modified_by = '${user_data.id}' , updated_at = '${datetime}'`,
+        values = null
+        whr=`bank_id=${value.bank_id}`
+        var insmd_bank = await db_Insert(table_name, fields, values, whr, 1)
+        // req.flash('success', "Bank Add Successful")
+        res.redirect('/super-admin/bank')
+
+    } catch (error) {
+        // req.flash('error', error)
+        // res.redirect('/super-admin/bank')
+        // return res.status(400).json({ error: error });
+    }
+
+}
+
+const admin_bank_list = async (req, res) => {
+    res.render('super-admin/admin_branch')
+}
+
+
+
+
+module.exports = { bank_list, add_bank_list,edit_bank_list,edit_bank_list_save, admin_bank_list}
--- routes/Sadmin.routes.js
+++ routes/Sadmin.routes.js
@@ -3,7 +3,7 @@ const { dashboard } = require("../controller/admin/Dashboard");
 const { agent_list } = require("../controller/superAdmin/Agent.controller");
 const { AuthCheckedMW } = require("../middleware/AuthCheckedMW");
 const { fetch_bank_info } = require("../controller/superAdmin/FetchData.controller");
-const { bank_list, add_bank_list } = require("../controller/superAdmin/Bank.controller");
+const { bank_list, add_bank_list, edit_bank_list, edit_bank_list_save, admin_bank_list } = require("../controller/superAdmin/Bank.controller");
 
 const Sadmin = express.Router()
 
@@ -16,6 +16,9 @@ Sadmin.post('/fetch_bank_info',AuthCheckedMW,fetch_bank_info)
 
 Sadmin.get('/bank',AuthCheckedMW,bank_list)
 Sadmin.post('/add_bank',AuthCheckedMW,add_bank_list)
+Sadmin.get('/edit_bank', edit_bank_list)
+Sadmin.post('/edit_bank_save', edit_bank_list_save)
 
+// Sadmin.get('/admin_branch')
 
 module.exports={Sadmin}
