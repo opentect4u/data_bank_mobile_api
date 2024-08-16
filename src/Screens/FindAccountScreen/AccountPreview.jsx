aed1db5207cf427ec9ec928f0e10b91def94192e
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-08-16 12:43:40 +0530

    Daily last tnx date added

--- src/Screens/FindAccountScreen/AccountPreview.jsx
+++ src/Screens/FindAccountScreen/AccountPreview.jsx
@@ -64,8 +64,13 @@ const AccountPreview = ({ navigation, route }) => {
     allowCollectionDays,
     secAmtType,
     razorpayInitializationJson,
+    bankId,
+    branchCode,
   } = useContext(AppStore)
+
   const { item, money } = route.params
+  const [lastTnxDate, setLastTnxDate] = useState(() => "")
+
   const tableData = [
     [
       "A/c Type",
@@ -83,8 +88,8 @@ const AccountPreview = ({ navigation, route }) => {
     ["Opening Date", new Date(item?.opening_date).toLocaleDateString("en-GB")],
     [
       "Previous Transaction Date",
-      item?.last_trns_dt
-        ? new Date(item?.last_trns_dt).toLocaleDateString("en-GB")
+      lastTnxDate
+        ? new Date(lastTnxDate).toLocaleDateString("en-GB")
         : "No available date",
     ],
     ["Previous Balance", item?.current_balance],
@@ -117,6 +122,45 @@ const AccountPreview = ({ navigation, route }) => {
   var tnxResponse
 
   const resetAction = StackActions.popToTop()
+
+  const getLastTnxDate = async () => {
+    const obj = {
+      bank_id: bankId,
+      branch_code: branchCode,
+      agent_code: userId,
+      account_number: item?.account_number,
+      flag: "D",
+    }
+
+    console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOO", obj)
+
+    await axios
+      .post(address.LAST_TNX_DATE, obj, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then(res => {
+        setLastTnxDate(
+          res?.data?.success?.length !== 0
+            ? res?.data?.success?.msg[0]?.last_trns_dt?.toString()
+            : "",
+        )
+
+        // {"status": true, "success": []}
+        console.log(
+          ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+          res?.data?.success?.msg[0]?.last_trns_dt,
+        )
+      })
+      .catch(err => {
+        console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<", err)
+      })
+  }
+
+  useEffect(() => {
+    getLastTnxDate()
+  }, [])
 
   const sendCollectedMoney = async () => {
     setLoading(true)
